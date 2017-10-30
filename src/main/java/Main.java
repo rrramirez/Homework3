10a2a7a7d34617182743228e4a5ec689214d1498
Ramon <ramirez.roberto45@gmail.com>
2017-10-29 22:44:09 -0500

    fixed program to be more like assignment0 aka HW1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -73,21 +73,21 @@ public class Main
 
         Comparable key = tempKey;
 
-        mergeSort(list, indexList,0, size-1);
+        //mergeSort(list, indexList,0, size-1);
 
 
         int returnValue = binSearch(list, indexList, key);
 
-        if(returnValue != -1)
-            returnValue = findDuplicates(list, indexList, returnValue);
+//        if(returnValue != -1)
+//            returnValue = findDuplicates(list, indexList, returnValue);
 
-        String result = "The key was not found!";
+        //String result = "The key was not found!";
 
-        if(-1<returnValue)
-            result = String.valueOf(indexList[returnValue]);
+//        if(-1<returnValue)
+//            result = String.valueOf(indexList[returnValue]);
 
 
-        out.println(result);
+        out.println(returnValue);
     }
 
     private static int binSearch(Comparable[] aList, int[] indexList, Comparable key)
@@ -101,7 +101,7 @@ public class Main
             mid = (min + max) /2;
             if(/*Double.compare(aList[mid], key)*/aList[mid].compareTo(key) == 0)
             {
-                return mid;
+                return 1;
             }
             else if(/*aList[mid] > key*/ aList[mid].compareTo(key) > 0)
             {
@@ -112,126 +112,126 @@ public class Main
                 min = mid + 1;
             }
         }
-        return -1;
+        return 0;
     }
 
-    private static void mergeSort(Comparable[] list, int[] indexList, int l, int h)
-    {
-        if(l < h) {
-            int mid = ((h - l) / 2) + l;
-
-            mergeSort(list, indexList, l, mid);
-            mergeSort(list,indexList,mid+1, h);
-
-            merge(list, indexList,l, mid, h);
-        }
-    }
-
-    private static void merge(Comparable[] list, int[] indexList, int l, int mid, int h)
-    {
-        int n1 = mid - l +1;
-        int n2 = h - mid;
-
-        Comparable[] listA = new Comparable[n1];
-        Comparable[] listB = new Comparable[n2];
-
-        int[] indexA = new int[n1];
-        int[] indexB = new int[n2];
-
-        for (int i=0; i < n1; i++)
-        {
-            listA[i] = list[i+l];
-            indexA[i] = indexList[i+l];
-        }
-        for(int j=0; j < n2; j++)
-        {
-            listB[j] = list[mid+1+j];
-            indexB[j] = indexList[mid+1+j];
-        }
-
-        int i=0, j=0, k=l;
-
-        while(i<n1 && j<n2)
-        {
-            if(/*listA[i] <= listB[j]*/ listA[i].compareTo(listB[j]) <= 0)
-            {
-                list[k] = listA[i];
-                indexList[k] = indexA[i];
-                i++; k++;
-            }
-            else
-            {
-                list[k] = listB[j];
-                indexList[k] = indexB[j];
-                j++; k++;
-            }
-        }
-
-        while (i < n1)
-        {
-            list[k] = listA[i];
-            indexList[k] = indexA[i];
-            i++; k++;
-        }
-
-        while(j < n2)
-        {
-            list[k] = listB[j];
-            indexList[k] = indexB[j];
-            j++; k++;
-        }
-    }
-
-    private static int findDuplicates(Comparable[] list, int[] indexList, int ogIndex)
-    {
-        boolean rightDuplicate = true;
-        boolean leftDuplicate = true;
-
-        int pointerIndex = ogIndex;
-        int finalIndex = ogIndex;
-
-        do
-        {
-            ++pointerIndex;
-            if(pointerIndex >= list.length)
-                break;
-
-            if(/*Double.compare(list[pointerIndex], list[finalIndex])*/ list[pointerIndex].compareTo(list[finalIndex]) == 0)
-            {
-                if(indexList[pointerIndex] < indexList[finalIndex])
-                {
-                    finalIndex = pointerIndex;
-                }
-            }
-            else
-            {
-                rightDuplicate = false;
-            }
-
-        }while(rightDuplicate);
-
-        pointerIndex = ogIndex;
-
-        do {
-
-            --pointerIndex;
-            if(pointerIndex <= -1)
-                break;
-
-            if(/*Double.compare(list[pointerIndex], list[finalIndex])*/ list[pointerIndex].compareTo(list[finalIndex]) == 0)
-            {
-                if(indexList[pointerIndex] < indexList[finalIndex])
-                {
-                    finalIndex = pointerIndex;
-                }
-            }
-            else
-            {
-                leftDuplicate = false;
-            }
-
-        }while(leftDuplicate);
-
-        return finalIndex;
-    }
+//    private static void mergeSort(Comparable[] list, int[] indexList, int l, int h)
+//    {
+//        if(l < h) {
+//            int mid = ((h - l) / 2) + l;
+//
+//            mergeSort(list, indexList, l, mid);
+//            mergeSort(list,indexList,mid+1, h);
+//
+//            merge(list, indexList,l, mid, h);
+//        }
+//    }
+//
+//    private static void merge(Comparable[] list, int[] indexList, int l, int mid, int h)
+//    {
+//        int n1 = mid - l +1;
+//        int n2 = h - mid;
+//
+//        Comparable[] listA = new Comparable[n1];
+//        Comparable[] listB = new Comparable[n2];
+//
+//        int[] indexA = new int[n1];
+//        int[] indexB = new int[n2];
+//
+//        for (int i=0; i < n1; i++)
+//        {
+//            listA[i] = list[i+l];
+//            indexA[i] = indexList[i+l];
+//        }
+//        for(int j=0; j < n2; j++)
+//        {
+//            listB[j] = list[mid+1+j];
+//            indexB[j] = indexList[mid+1+j];
+//        }
+//
+//        int i=0, j=0, k=l;
+//
+//        while(i<n1 && j<n2)
+//        {
+//            if(/*listA[i] <= listB[j]*/ listA[i].compareTo(listB[j]) <= 0)
+//            {
+//                list[k] = listA[i];
+//                indexList[k] = indexA[i];
+//                i++; k++;
+//            }
+//            else
+//            {
+//                list[k] = listB[j];
+//                indexList[k] = indexB[j];
+//                j++; k++;
+//            }
+//        }
+//
+//        while (i < n1)
+//        {
+//            list[k] = listA[i];
+//            indexList[k] = indexA[i];
+//            i++; k++;
+//        }
+//
+//        while(j < n2)
+//        {
+//            list[k] = listB[j];
+//            indexList[k] = indexB[j];
+//            j++; k++;
+//        }
+//    }
+//
+//    private static int findDuplicates(Comparable[] list, int[] indexList, int ogIndex)
+//    {
+//        boolean rightDuplicate = true;
+//        boolean leftDuplicate = true;
+//
+//        int pointerIndex = ogIndex;
+//        int finalIndex = ogIndex;
+//
+//        do
+//        {
+//            ++pointerIndex;
+//            if(pointerIndex >= list.length)
+//                break;
+//
+//            if(/*Double.compare(list[pointerIndex], list[finalIndex])*/ list[pointerIndex].compareTo(list[finalIndex]) == 0)
+//            {
+//                if(indexList[pointerIndex] < indexList[finalIndex])
+//                {
+//                    finalIndex = pointerIndex;
+//                }
+//            }
+//            else
+//            {
+//                rightDuplicate = false;
+//            }
+//
+//        }while(rightDuplicate);
+//
+//        pointerIndex = ogIndex;
+//
+//        do {
+//
+//            --pointerIndex;
+//            if(pointerIndex <= -1)
+//                break;
+//
+//            if(/*Double.compare(list[pointerIndex], list[finalIndex])*/ list[pointerIndex].compareTo(list[finalIndex]) == 0)
+//            {
+//                if(indexList[pointerIndex] < indexList[finalIndex])
+//                {
+//                    finalIndex = pointerIndex;
+//                }
+//            }
+//            else
+//            {
+//                leftDuplicate = false;
+//            }
+//
+//        }while(leftDuplicate);
+//
+//        return finalIndex;
+//    }
 }
